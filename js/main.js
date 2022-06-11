2a736ef424cac91ab17e8280aca22083cd3634f2
Elizabeth Oteje <lizzyoteje@gmail.com>
2022-06-11 12:53:37 +0100

    This is last commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
-const modal = document.querySelector('.modal')
-const loginBtn = document.querySelector('.login-btn')
-const closeBtn = document.querySelector('.close')
 
-loginBtn.addEventListener('click', openModal)
-closeBtn.addEventListener('click', closeModal)
-window.addEventListener('click', outsideClick)
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+
+
+// When the user clicks on the button, open the modal
+btn.addEventListener('click', openModal);
+span.addEventListener('click', closeModal);
+window.addEventListener('click', windowOnClick);        
+                                                                   
+
 
 function openModal() {
-   modal.style.display = 'block'
+  // modal.classList.toggle("modal-box");
+  modal.style.display = 'block'
+  // console.log('clicked');
 }
 
 function closeModal() {
-    modal.style.display = 'none'
+  modal.style.display = 'none'
 }
 
-function outsideClick(e) {
-    if(e.target == modal) {
-        closeModal()
-    }
+function windowOnClick(event) {
+  if (event.target === modal) {
+      closeModal();
+  }
 }
+
